a37a4ba61c37c625f78eab3fd399e26f0e4aff98
markolchanyi <olchanyi@mit.edu>
2023-02-19 12:36:17 -0500

    removed some stray files

--- run_EXC_twocases.sh
+++
@@ -1,4 +0,0 @@
-#python CRSEG_main_multithreaded.py -at ../../../Atlases/CRSEG_atlas -t text_files/EXC_case_012.txt -r 0.75 -n 1 -o 0.45
-#python CRSEG_main_multithreaded.py -at ../../../Atlases/CRSEG_atlas -t text_files/EXC_case_007.txt -r 0.75 -n 1 -o 0.45
-#python CRSEG_main_multithreaded.py -at ../../../Atlases/CRSEG_atlas -t text_files/EXC_case_C1.txt -r 0.75 -n 1 -o 0.45
-python CRSEG_main_multithreaded.py -at ../../../Atlases/CRSEG_atlas -t text_files/EXC_case_020.txt -r 0.75 -n 1 -o 0.45
