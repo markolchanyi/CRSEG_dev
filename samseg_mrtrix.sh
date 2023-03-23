d11722ae17a6caabc925fd180ab5990a09b69250
markolchanyi <olchanyi@mit.edu>
2023-03-23 18:21:00 -0400

    empty commit

--- samseg_mrtrix.sh
+++
@@ -1,6 +0,0 @@
-#source freeurfer for samseg
-export FREESURFER_HOME="/usr/local/freesurfer/7.2.0"
-source $FREESURFER_HOME/SetUpFreeSurfer.sh
-
-#run mrtrix preprocessing
-python trackgen.py --basepath /autofs/space/nicc_003/users/olchanyi/data/HCP100/ --caselist text_files/case_list_shortened.txt --datapath T1w/Diffusion/data.nii.gz --bvalpath T1w/Diffusion/bvals --bvecpath T1w/Diffusion/bvecs --cropsize 56 --output mrtrix_outputs
